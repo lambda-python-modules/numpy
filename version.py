365eb6b8cc13bb9a81c1a38861976742b96b5b43
EC2 Default User <ec2-user@ip-172-31-17-205.ap-southeast-2.compute.internal>
2017-05-18 00:21:18 +0000

    First commit

---
+++ version.py
@@ -0,0 +1,12 @@
+
+# THIS FILE IS GENERATED FROM NUMPY SETUP.PY
+#
+# To compare versions robustly, use `numpy.lib.NumpyVersion`
+short_version = '1.13.0rc1'
+version = '1.13.0rc1'
+full_version = '1.13.0rc1'
+git_revision = 'd6ab6da640156e4a75a78d5d6e7f25a098b5f52b'
+release = True
+
+if not release:
+    version = full_version
